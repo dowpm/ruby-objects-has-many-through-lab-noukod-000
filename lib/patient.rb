7554c2e503a538512270ac3be9d597c1c65af431
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-03 19:57:18 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,7 +19,7 @@ class Patient
     Appointment.all.select {|appointment| appointment.patient == self}
   end
 
-  def patients
+  def doctors
     appointments.map {|appointment| appointment.doctor}
   end
 
